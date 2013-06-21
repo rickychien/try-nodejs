68e83f39d54d85a80742d4e3a0f0f441d85064e2
Ricky Chien <ricky060709@gmail.com>
2013-06-21 23:33:08 +0800

    Add upload jpg functionality.

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -1,6 +1,8 @@
 var querystring = require("querystring");
+var fs = require('fs');
+var formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
@@ -9,25 +11,52 @@ function start(response, postData) {
     'charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
+  response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text: " + 
-    querystring.parse(postData).text);
-  response.end();
+
+  var form = new formidable.IncomingForm();
+  form.parse(request, function(error, fields, files) {
+    if (error) {
+      console.log("data parsing error: " + error);
+    } else {
+      console.log("image data parsing done");
+      fs.renameSync(files.upload.path, "/tmp/test.jpg");
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write("received image:<br/>");
+      response.write("<img src='/show' />");
+      response.end();
+    }
+  });
+}
+
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("/tmp/test.jpg", "binary", function(error, file) {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/jpg"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
