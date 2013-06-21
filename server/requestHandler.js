3e37b0c58ef977027ee9a0de820ead507be0a4ee
Ricky Chien <ricky060709@gmail.com>
2013-06-21 18:46:54 +0800

    Add non-blocking way to 'ls' directory content.

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -1,9 +1,21 @@
-function start() {
+var child_process = require("child_process");
+
+function start(response) {
   console.log("Request handler 'start' was called.");
+
+  child_process.exec("ls -lah", function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
+  });
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
+
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
