ea26af7f7261a369fb9887d9ca9ab09d8dbf3829
Ricky Chien <ricky060709@gmail.com>
2013-06-21 18:26:08 +0800

    Fix typo error in index.js and add .gitignore.

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ var router = require('./router');
 var requestHandler = require('./requestHandler');
 
 var handle = {}
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
-handle["/upload"] = requestHandlers.upload;
+handle["/"] = requestHandler.start;
+handle["/start"] = requestHandler.start;
+handle["/upload"] = requestHandler.upload;
 
 server.start(router.route, handle);
