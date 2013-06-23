24646f334bc5f4e5f169153793641ef212b736ff
Ricky Chien <ricky060709@gmail.com>
2013-06-23 15:52:43 +0800

    Remove local mocha.

--- test/server.js
+++ test/server.js
@@ -5,33 +5,9 @@ var server = require('../server/server');
 
 var url = "http://localhost:8888";
 
-describe('GET', function() {
-  describe('/', function() {
-    it('should return request object', function() {
-      browser.visit(url + "/");
-
-      assert.notEqual(undefined, server);
-    });
-  });
-
-  describe('/start', function() {
-    it('should return request object', function() {
-      browser.visit(url + "/");
-
-      assert.notEqual(undefined, server);
-    });
-  });
-
-  describe('/upload', function() {
-    it('should return request object', function() {
-      browser.visit(url + "/");
-
-      assert.notEqual(undefined, server);
-    });
-  });
-
-  describe('/show', function() {
-    it('should return request object', function() {
+suite('GET', function() {
+  suite('/', function() {
+    test('should return request object', function() {
       browser.visit(url + "/");
 
       assert.notEqual(undefined, server);
