51ce33914e3a67ce6a4515a408d740c664c84c72
Ricky Chien <ricky060709@gmail.com>
2013-06-26 17:40:11 +0800

    Modify /upload test suite.

--- test/index.js
+++ test/index.js
@@ -1,38 +1,67 @@
 var Browser = require('zombie'),
-    browser = new Browser(),
     assert = require('assert'),
     server = require('../server/index');
 
-suite('GET', function() {
-  before(function() {
+suite('Index', function() {
+  'use strict';
+
+  suiteSetup(function() {
     this.browser = new Browser({ site: 'http://localhost:8888' });
   });
 
-  suite('/', function() {
-    beforeEach(function(done) {
+  suite('Visit /', function() {
+    suiteSetup(function(done) {
       this.browser.visit('/', done);
     });
 
-    test('should return success status code 2xx', function() {
+    test('return success status code 2xx', function() {
       assert.ok(this.browser.success);
     });
 
-    test('should contain a form', function() {
+    test('contain a form', function() {
       assert.ok(this.browser.query('form'));
     });
   });
 
-  suite('/start', function() {
-    beforeEach(function(done) {
+  suite('Visit /start', function() {
+    suiteSetup(function(done) {
       this.browser.visit('/start', done);
     });
 
-    test('should return success status code 2xx', function() {
+    test('return success status code 2xx', function() {
       assert.ok(this.browser.success);
     });
 
-    test('should contain a form', function() {
+    test('contain a form', function() {
       assert.ok(this.browser.query('form'));
+    });
+  });
+
+  suite('Visit /upload', function() {
+    test('return success status code 2xx', function(done) {
+      this.browser.visit('/upload', function() {
+        assert.ok(this.browser.success);
+        done();
+      }.bind(this));
+    });
+
+    test('show an error message with directly visit', function(done) {
+      this.browser.visit('/upload', function() {
+        assert.equal(this.browser.text('body'), 'upload data error, please retry again.');
+        done();
+      }.bind(this));
+    });
+
+    test('test upload image', function(done) {
+      this.browser.visit('/', function() {
+        this.browser
+          .attach('input[type=file]', '/tmp/test.jpg')
+          .pressButton('input[type=submit]', function() {
+            assert.equal(this.browser.location.pathname, '/upload');
+            assert.ok(this.browser.query('img[src="/show"]'));
+            done();
+          }.bind(this));
+      }.bind(this));
     });
   });
 });
